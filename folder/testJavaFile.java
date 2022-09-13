011fdf86ddc9e3df34898108faa48ac593c53ef2
CapybaraJJ <joeljohnston101@gmail.com>
2022-09-13 13:56:03 -0400

    dek anymore

---
+++ folder/testJavaFile.java
@@ -0,0 +1,16 @@
+import java.util.Scanner
+
+public class test{
+	public static void main[String args]{
+		
+		scanner sc = new Scanner(System.in);
+
+		system.out.println("hello world");
+		system.out.println("please enter an integer: ");
+
+		int number = Sc.nextInt;
+
+		System.out.println("your number is : " + number);
+
+	}
+}
